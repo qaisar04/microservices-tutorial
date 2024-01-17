885a59f20c6bd0c6d7ac95549c5673931ac8fd76
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-01-17 23:26:10 +0600

    kafka impl

---
+++ notification-service/src/main/java/kz/baltabayev/model/event/OrderPlacedEvent.java
@@ -0,0 +1,12 @@
+package kz.baltabayev.model.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
